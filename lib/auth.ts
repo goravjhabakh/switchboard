f1def2ee7c1e71b6e99504e1d67354fb6951c62e
goravjhabakh <goravjhabakh@gmail.com>
2026-01-28 21:26:38 +0530

    09: payments

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth"
+import { polar, checkout, portal, usage, webhooks } from '@polar-sh/better-auth'
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import prisma from "./prisma"
+import { polarClient } from "./polar"
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -19,5 +21,22 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }
-  }
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [{
+            productId: "ac02689c-d21c-45e8-809e-a6dac2259da2",
+            slug: "Switch-Board-Pro"
+          }],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true
+        }),
+        portal()
+      ]
+    })
+  ]
 })
